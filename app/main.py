c8cdadb74641cee7919dc4d11f5391ce30ad6132
romdle <vovaikzakov@gmail.com>
2025-10-29 07:06:23 +0300

    12223

--- app/main.py
+++ app/main.py
@@ -1,33 +1,22 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-import json
-import os
+from fastapi.staticfiles import StaticFiles
+
+from routers import pages
 
 app = FastAPI()
 
-@app.get("/")
-async def get_data():
-    path = os.path.join(os.path.dirname(__file__), "data", "data.json")
-    with open(path, encoding="utf-8") as f:
-        data = json.load(f)
-    return JSONResponse(content=data)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/category/{name}")             # это ссылка под каталог, изначально будет открываться один, а дальше все остальные через кнопки в интерфейсе
-async def category(name: str):
-    return f"Категория: {name}"
-
-@app.get("/product/{name}")               
-async def product(name: str):
-    return {f"Страница товара №{name}": "work"}
-
-@app.get("/company")
-async def print():
-    return {"company" : "work"}
-
-@app.get("/contacts")
-async def print():
-    return {"contacts" : "work"}
+app.include_router(pages.router)
+# app.include_router(api.router)
 
 
 
-## запуск локалки через команду      uvicorn main:app --reload    в директории backend
+
+
+
+
+
+
+
+## запуск локалки через команду      uvicorn main:app --reload    в директории backend\
---
+++ app/routers/pages.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import json, os
+
+templates = Jinja2Templates(directory="templates")
+router = APIRouter() 
+
+@router.get("/", response_class=HTMLResponse)
+async def get_data(request: Request):
+    return templates.TemplateResponse("glavnaya.html", {"request": request})
+
+@router.get("/category/{name}")             
+async def category(name: str):
+    return f"Категория: {name}"
+
+@router.get("/product/{name}")               
+async def product(name: str):
+    return {f"Страница товара №{name}": "work"}
+
+@router.get("/info", response_class=HTMLResponse)
+async def company(request: Request):
+    return templates.TemplateResponse("info.html", {"request": request})
+
+@router.get("/contacts", response_class=HTMLResponse)
+async def contacts(request: Request):
+    return templates.TemplateResponse("contacts.html", {"request": request})
