c7a79d7e63573c4c5ded8d430c9e909df1180825
romdle <vovaikzakov@gmail.com>
2025-11-03 19:47:51 +0300

    vernul k test 7, a to pizdec

--- app/routers/pages.py
+++ app/routers/pages.py
@@ -16,32 +16,12 @@ router = APIRouter()
 async def get_data(request: Request):
     return templates.TemplateResponse("glavnaya.html", {"request": request})
 
-
-@router.get("/catalog", response_class=HTMLResponse)
-async def catalog_page(request: Request):
-    return templates.TemplateResponse("glavnaya.html", {"request": request})
-
-
-@router.get("/about", response_class=HTMLResponse)
-async def about_page(request: Request):
-    return templates.TemplateResponse("glavnaya.html", {"request": request})
-
-
-@router.get("/contacts", response_class=HTMLResponse)
-async def contacts_page(request: Request):
-    return templates.TemplateResponse("glavnaya.html", {"request": request})
-
-
-@router.get("/product/{path_id}", response_class=HTMLResponse)
-async def product_page(path_id: str, request: Request):
-    return templates.TemplateResponse("glavnaya.html", {"request": request})
-
 @router.get("/category/{name}")             
 async def category(name: str):
     return f"Категория: {name}"
 
 
-@router.get("/api/products")
+@router.get("/products")
 async def list_products(
     db: sqlite3.Connection = Depends(get_db),
 ):
@@ -49,7 +29,7 @@ async def list_products(
     return {"items": products}
 
 
-@router.get("/api/product/{product_id}")
+@router.get("/product/{product_id}")
 async def product(
     product_id: int,
     db: sqlite3.Connection = Depends(get_db),
@@ -58,3 +38,11 @@ async def product(
     if product_row is None:
         raise HTTPException(status_code=404, detail="Product not found")
     return dict(product_row)
+
+@router.get("/info", response_class=HTMLResponse)
+async def company(request: Request):
+    return templates.TemplateResponse("info.html", {"request": request})
+
+@router.get("/contacts", response_class=HTMLResponse)
+async def contacts(request: Request):
+    return templates.TemplateResponse("contacts.html", {"request": request})
